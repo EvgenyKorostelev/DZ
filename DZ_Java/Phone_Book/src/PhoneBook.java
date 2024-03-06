e8bc86a315da8e1d60e75179d53c05408ebe0934
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-03-06 12:12:12 +0300

    fix_sort_in_DZjava_PhoneBook

--- DZ_Java/Phone_Book/src/PhoneBook.java
+++ DZ_Java/Phone_Book/src/PhoneBook.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class PhoneBook {
     private static HashMap<String, ArrayList<Integer>> phoneBook = new HashMap<>();
-
+//  метод добавления в книгу
     public void add(String name, Integer phoneNum) {
         if (phoneBook.containsKey(name)) {
             ArrayList<Integer> list = phoneBook.get(name);
@@ -14,7 +14,7 @@ public class PhoneBook {
             phoneBook.put(name, list);
         }
     }
-
+//  метод поиска в книге
     public HashMap<String, ArrayList<Integer>> find(String name) {
         HashMap<String, ArrayList<Integer>> map = new HashMap<>();
         if (phoneBook.get(name) != null) {
@@ -23,17 +23,17 @@ public class PhoneBook {
         }
         return map;
     }
-
+//  метод вывода/возврата книги(без сортировки)
     public static HashMap<String, ArrayList<Integer>> getPhoneBook(PhoneBook temp) {
         return phoneBook;
     }
-
+//  метод вывода книги (сортировка по убыванию количества номеров)
     public static void outSortPBook(PhoneBook temp) {
         ArrayList<ArrayList<Integer>> val = new ArrayList<>(phoneBook.values());
         val.sort(new Comparator<ArrayList<Integer>>() {
             @Override
             public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-                return o1.size() - o2.size();
+                return o2.size() - o1.size();
             }
         });
         for (ArrayList<Integer> value : val) {
