c0d4d7da69783bf17b7b96b04c810ff1b500c996
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-04-21 11:18:41 +0300

    fix_idFactory_for_Notes

--- DZ_Python/Notes/Entity/idFactory.py
+++ DZ_Python/Notes/Entity/idFactory.py
@@ -1,7 +1,14 @@
-temp_id = 0
-
-
 def create_id():
-    global temp_id
-    temp_id += 1
-    return temp_id
+    try:
+        file = open('id_counter.csv', 'r+')
+    except IOError:
+        file = open('id_counter.csv', 'w+')
+    id_group = file.readlines()
+    if len(id_group) == 0:
+        id = 1
+        file.write(f"{id + 1}")
+    else:
+        id = int(id_group[-1])
+        file.write(f"\n{id + 1}")
+    file.close()
+    return id
